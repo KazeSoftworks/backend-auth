6d3cd3ae2cc62e95046a00f1a767f74f19eb7bdd
Spellkaze <spellkaze@live.com>
2021-11-08 14:59:03 -0500

    [ADD] Passport jwt protection in route Categories

--- token-verify.js
+++ token-verify.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const secret = 'lolCat';
 const token =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInJvbGUiOiJjdXN0b21lciIsImlhdCI6MTYzNDcwNDgyMH0.mLxsjkV5hJQbU_fvkOM8kbZjwSIww9mxOD5XX5t2Jns';
+	'https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInJvbGUiOiJjdXN0b21lciIsImlhdCI6MTYzNDcwNDgyMH0.mLxsjkV5hJQbU_fvkOM8kbZjwSIww9mxOD5XX5t2Jns';
 
 const verifyToken = (token, secret) => {
 	return jwt.verify(token, secret);
