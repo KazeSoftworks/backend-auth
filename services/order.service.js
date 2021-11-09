cd9dc05266d501e80c8fba0a21d2535174812a05
Spellkaze <spellkaze@live.com>
2021-11-09 11:30:29 -0500

    [ADD] Login auth jwt token

--- services/order.service.js
+++ services/order.service.js
@@ -3,48 +3,60 @@ const boom = require('@hapi/boom');
 const { models } = require('./../libs/sequelize');
 
 class OrderService {
+	constructor() {}
 
-  constructor(){
-  }
+	async create(data) {
+		const newOrder = await models.Order.create(data);
+		return newOrder;
+	}
 
-  async create(data) {
-    const newOrder = await models.Order.create(data);
-    return newOrder;
-  }
+	async addItem(data) {
+		const newItem = await models.OrderProduct.create(data);
+		return newItem;
+	}
 
-  async addItem(data) {
-    const newItem = await models.OrderProduct.create(data);
-    return newItem;
-  }
+	async find() {
+		return [];
+	}
 
-  async find() {
-    return [];
-  }
+	async findByUser(userId) {
+		const orders = await models.Order.findAll({
+			where: {
+				'$customer.user.id$': userId,
+			},
+			include: [
+				{
+					association: 'customer',
+					include: ['user'],
+				},
+			],
+		});
+		return orders;
+	}
 
-  async findOne(id) {
-    const order = await models.Order.findByPk(id, {
-      include: [
-        {
-          association: 'customer',
-          include: ['user']
-        },
-        'items'
-      ]
-    });
-    return order;
-  }
+	async findOne(id) {
+		const order = await models.Order.findByPk(id, {
+			include: [
+				{
+					association: 'customer',
+					include: ['user'],
+				},
+				'items',
+			],
+		});
+		return order;
+	}
 
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
-  }
+	async update(id, changes) {
+		return {
+			id,
+			changes,
+		};
+	}
 
-  async delete(id) {
-    return { id };
-  }
-
+	async delete(id) {
+		return { id };
+	}
 }
 
 module.exports = OrderService;
